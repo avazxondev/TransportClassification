6727eb3b1b0dbeb891905b210ebc7aba6f90cd14
Avazkhan <123217986+avazxondev@users.noreply.github.com>
2024-11-17 00:46:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 from fastai.vision.all import *
 import pathlib
-import plotly.express as px 
-temp = pathlib.PosixPath
-pathlib.PosixPath = pathlib.WindowsPath
+import plotly.express as px
 
+plt = platform.sydtem()
+if plt == "Linux": pathlib.WindowsPath = pathlib.PosixPath
+    
 #title
 st.title('The model of CalassificationVehicles')
 
